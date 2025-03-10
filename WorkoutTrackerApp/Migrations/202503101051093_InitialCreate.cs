842e0686a523515cbe2603a60202ae729eda011d
Samuel Kocych <samuel@kocych.cz>
2025-03-10 11:08:01 +0000

    working on new training page xaml

---
+++ WorkoutTrackerApp/Migrations/202503101051093_InitialCreate.cs
@@ -0,0 +1,46 @@
+﻿namespace WorkoutTrackerApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Exercises",
+                c => new
+                    {
+                        ExerciseID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Sets = c.Int(nullable: false),
+                        Reps = c.Int(nullable: false),
+                        Weight = c.Double(nullable: false),
+                        WorkoutID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ExerciseID)
+                .ForeignKey("dbo.Workouts", t => t.WorkoutID, cascadeDelete: true)
+                .Index(t => t.WorkoutID);
+            
+            CreateTable(
+                "dbo.Workouts",
+                c => new
+                    {
+                        WorkoutID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Date = c.DateTime(nullable: false),
+                        TotalDuration = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.WorkoutID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Exercises", "WorkoutID", "dbo.Workouts");
+            DropIndex("dbo.Exercises", new[] { "WorkoutID" });
+            DropTable("dbo.Workouts");
+            DropTable("dbo.Exercises");
+        }
+    }
+}
