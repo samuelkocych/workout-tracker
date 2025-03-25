94d5833207dfa0dbf9c90078ae7d35d6b01be20a
Samuel Kocych <samuel@kocych.cz>
2025-03-25 21:56:14 +0000

    started working on testing

---
+++ WorkoutTracker.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WorkoutTracker.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WorkoutTracker.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("726dbed8-1b86-453b-87d6-2b9ab23331e5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WorkoutTracker.Tests/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WorkoutTrackerApp;
+
+namespace WorkoutTracker.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // Exercise class tests
+        [TestMethod]
+        public void TestExerciseCreatesCorrectly()
+        {
+            // arrange
+            var exercise = new Exercise("Bench Press", 3, 10, 60.0);
+
+            // assert
+            Assert.AreEqual("Bench Press", exercise.Name);
+            Assert.AreEqual(3, exercise.Sets);
+            Assert.AreEqual(10, exercise.Reps);
+            Assert.AreEqual(60.0, exercise.Weight);
+        }
+
+        [TestMethod]
+        public void TestExerciseToString()
+        {
+            // arrange
+            var exercise = new Exercise("Squat", 4, 8, 70.0);
+
+            // act
+            var result = exercise.ToString();
+
+            // assert
+            Assert.AreEqual("Squat - 4 Sets x 8 Reps x 70 kg", result);
+        }
+
+
+        // Workout class tests
+        [TestMethod]
+        public void TestWorkoutAddsExercise()
+        {
+            // arrange
+            var workout = new Workout();
+            var exercise = new Exercise("Deadlift", 5, 5, 100.0);
+
+            // act
+            workout.Exercises.Add(exercise);
+
+            // assert
+            Assert.AreEqual(1, workout.Exercises.Count);
+            Assert.AreEqual("Deadlift", workout.Exercises[0].Name);
+        }
+    }
+}
